63683d71645163c073f49a109f010d2926e2abf3
soumikmickey <soumikbanerjee@hotmail.com>
2024-10-16 15:45:44 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,50 @@
+# Function to perform basic arithmetic operations
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error! Division by zero."
+
+# Main program
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+while True:
+    # Take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # Check if the choice is valid
+    if choice in ['1', '2', '3', '4']:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(f"The result is: {add(num1, num2)}")
+
+        elif choice == '2':
+            print(f"The result is: {subtract(num1, num2)}")
+
+        elif choice == '3':
+            print(f"The result is: {multiply(num1, num2)}")
+
+        elif choice == '4':
+            print(f"The result is: {divide(num1, num2)}")
+        
+        # Ask if the user wants to continue
+        next_calculation = input("Do you want to perform another calculation? (yes/no): ")
+        if next_calculation.lower() != 'yes':
+            break
+    else:
+        print("Invalid Input")
